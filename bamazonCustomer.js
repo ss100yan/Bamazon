bc643a29958ede9b9f08af0744cf22b0bde2b509
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-03-04 11:23:54 -0500

    new

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,31 @@
 
 var mysql = require('mysql');
-var Inquirer = require('inquirer');
+var inquirer = require('inquirer');
+
+var questions = [
+    {
+        type: 'input',
+        name: 'ID',
+        message: "Please enter the ID of the product they would like to buy !",
+        validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+          },
+          filter: Number
+      },
+      {
+        type: 'input',
+        name:'Quantity' ,
+        message: "How many units of the product they would like to buy?",
+        vvalidate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+          },
+          filter: Number
+      },
+
+
+];
 
 
 
@@ -17,5 +42,19 @@ var con = mysql.createConnection({
     con.query("SELECT * FROM products", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
+
+              inquirer
+        .prompt(questions)
+        .then(answers => {
+          // Use user feedback for... whatever!! 
+
+               console.log('\nOrder receipt:');
+               console.log(JSON.stringify(answers, null, '  '));
+        });
+
+        
+
+       
       });
   });
+ 
